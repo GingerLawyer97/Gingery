8f50b1bfb301bff477464d2ed2a193f0e0d51931
GingerLawyer97 <gingerlawyer97@gmail.com>
2024-04-07 20:55:17 +0530

    Added Application Commands System

--- gingery.py
+++ gingery.py
@@ -4,20 +4,32 @@ import discord
 from discord import app_commands
 
 intents = discord.Intents.default()
-intents.message_content = True
-
 client = discord.Client(intents=intents)
+
+intents.message_content = True
+tree = app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# Add the guild ids in which the slash command will appear.
+# If it should be in all, remove the argument, but note that
+# it will take some time (up to an hour) to register the
+# command if it's for all guilds.
+@tree.command(
+    name="commandname",
+    description="My first application Command",
+    # guild=discord.Object(id=12417128931)
+)
+async def first_command(interaction):
+    await interaction.response.send_message("Hello!")
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@client.event
+async def on_ready():
+    await tree.sync(guild=discord.Object(id=Your guild id))
+    print("Ready!")
+
+
 
 client.run('MTIyNjQ2NzAzODExMzgyODg4NA.GZDDBi.R6Pu3j0LA0FcN8SdlzTWYua98Par-fOmOaPw0U')
