1c3055b3875b6f8a71974a91d1bf982895596dd7
GingerLawyer97 <gingerlawyer97@gmail.com>
2024-04-07 15:20:03 +0530

    Made simple bot

--- gingery.py
+++ gingery.py
@@ -1,1 +1,22 @@
-print("hello world")
+# This example requires the 'message_content' intent.
+
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('MTIyNjQ2NzAzODExMzgyODg4NA.GZDDBi.R6Pu3j0LA0FcN8SdlzTWYua98Par-fOmOaPw0U')
