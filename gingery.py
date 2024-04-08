f537a5fa22ee53b83fbe4b824d251fc139ff6ba4
GingerLawyer97 <gingerlawyer97@gmail.com>
2024-04-08 15:49:52 +0530

    Changed Bot Token

--- gingery.py
+++ gingery.py
@@ -30,4 +30,4 @@ async def on_ready():
     await tree.sync()
     print("Ready!")
 
-client.run('MTIyNjQ2NzAzODExMzgyODg4NA.GZDDBi.R6Pu3j0LA0FcN8SdlzTWYua98Par-fOmOaPw0U')
+client.run('MTIyNjQ2NzAzODExMzgyODg4NA.GL_kq4.Qvb9L5iXPzQC5cvCdGfeDpTQ7cj9EM5xi6eq8g')
