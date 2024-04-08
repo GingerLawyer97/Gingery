6191904395695080a91ea08b2e0bec74b30a07ac
GingerLawyer97 <gingerlawyer97@gmail.com>
2024-04-08 15:43:03 +0530

    Added Simple Hello Cmd

--- gingery.py
+++ gingery.py
@@ -9,21 +9,21 @@ client = discord.Client(intents=intents)
 intents.message_content = True
 tree = app_commands.CommandTree(client)
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
 # Add the guild ids in which the slash command will appear.
 # If it should be in all, remove the argument, but note that
 # it will take some time (up to an hour) to register the
 # command if it's for all guilds.
 @tree.command(
-    name="commandname",
-    description="My first application Command",
+    name="hello",
+    description="Say Hello to the Bot",
     # guild=discord.Object(id=12417128931)
 )
 async def first_command(interaction):
     await interaction.response.send_message("Hello!")
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_ready():
