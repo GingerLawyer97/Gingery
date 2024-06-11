bcd4db1fa8b13e76afb347ca052a8ccc0633dc06
GingerLawyer97 <94100561+GingerLawyer97@users.noreply.github.com>
2024-06-11 06:17:46 +0000

    Added Token using Replit

--- gingery.py
+++ gingery.py
@@ -14,10 +14,15 @@ intents.message_content = True
 
 client = commands.Bot(command_prefix=['!'], intents=intents)
 
+
 @client.event
 async def on_ready():
     print('GingeryPy has connected to Discord!')
-    await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.listening, name="!help"))
+    await client.change_presence(status=discord.Status.idle,
+                                 activity=discord.Activity(
+                                     type=discord.ActivityType.listening,
+                                     name="!help"))
+
 
 @client.event
 async def on_message(message):
@@ -26,25 +31,29 @@ async def on_message(message):
 
     # Help Command
     if message.content.startswith('!help'):
-        embedvar = discord.Embed(title=">>>> HELP <<<<", description="List of Commands to use the Bot:")
+        embedvar = discord.Embed(
+            title=">>>> HELP <<<<",
+            description="List of Commands to use the Bot:")
         embedvar.add_field(name="`!coinflip`", value="- Flip's a Coin.")
         embedvar.add_field(name="`!rolladice`", value="- Roll's a Dice.")
-    
-    # Roll a Dice Command    
+
+    # Roll a Dice Command
     if message.content.startswith('!rolladice'):
-        radnum = randrange(0,7) 
+        radnum = randrange(0, 7)
         await message.channel.send("The Number is... ")
         time.sleep(1)
         await message.channel.send(radnum)
 
     # Coin Flip Command
     if message.content.startswith('!coinflip'):
-        coinflip = randrange(-1,2)
+        coinflip = randrange(-1, 2)
         await message.channel.send("Flipping... ")
         time.sleep(2)
         if coinflip == 1:
             await message.channel.send("Heads!")
         else:
             await message.channel.send("Tails!")
-            
-client.run("")
+
+
+token = os.environ['TOKEN']
+client.run(token)
